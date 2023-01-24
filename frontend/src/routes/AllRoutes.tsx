195435bfced17d51b8d4081fbfaa35ba8a864065
1Gireesh <gireeshbhat68@gmail.com>
2023-01-24 19:53:59 +0530

    added routes

---
+++ frontend/src/routes/AllRoutes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import Editor from '../pages/Editor'
+
+export default function AllRoutes() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home></Home>} />
+      <Route path='/editor/id' element={<Editor></Editor>} />
+
+    </Routes>
+  )
+}
