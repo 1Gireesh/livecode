29882cdaf169c91ee2eed9200eef8bea84c042f1
Soumitra-dey <soumitra.petbindhi@gmail.com>
2023-01-24 21:40:14 +0530

    complete home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
+import Home from './pages/Home';
 import AllRoutes from './routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <Nav></Nav>
-      <AllRoutes></AllRoutes>
+      {/* <Nav></Nav> */}
+      {/* <AllRoutes></AllRoutes> */}
+      <Home/>
     </div>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import "../style/home.css"
 
 export default function Home() {
+
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="form">
+        <div className="form_heading">
+          <h1>LiveCode</h1>
+        </div>
+        <div className="form_body">
+          <input type="text" placeholder='ROOM ID'/>
+          <input type="text" placeholder='USERNAME'/>
+          <button className="btn btn-5">Let's code</button>
+          <p>Create new invite ID?</p>
+        </div>
+      </div>
+    </div>
   )
 }
+
+
